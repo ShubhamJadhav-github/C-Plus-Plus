ed40e1ac05a97253e952a5da8bf80f994b39abdc
Shubham Jadhav <111007875+ShubhamJadhav-github@users.noreply.github.com>
2023-06-20 16:03:00 +0530

    1.HelloWorld.cpp
    1.HelloWorld.cpp

---
+++ 1.HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+// By including the using namespace std; 
+// At the beginning of your C++ code
+// You don't have to explicitly mention the std:: prefix 
+// Before using any standard library components. 
+// This simplifies the code and makes it more readable 
+// By eliminating the need for repetitive std:: prefixes.
+using namespace std;
+int main()
+{
+    // Character Output Stream (cout)
+    cout<<"Hello GUYS!!!";
+    return 0;
+}
